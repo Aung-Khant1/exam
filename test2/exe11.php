d8b68e8a265d4f2de464964520b5ee69cb9ddfa2
Woody <1122aungkhant@gmail.com>
2020-10-15 17:50:02 +0630

    exam

---
+++ test2/exe11.php
@@ -0,0 +1,30 @@
+<?php
+	$a=1000;
+	$b=1200;
+	$c=1400;
+	echo "<table border=1 cellspacing=0 cellpading=0 style='width:400px; line-height:50px; text-align:center; margin:0 auto;'>
+	<tr><td><span style='color:blue;'>Salary of Mr. A is</span></td><td>".$a." $</td></tr> 
+	<tr><td><span style='color:blue;'>Salary of Mr. B is</span></td><td>".$b." $</td></tr>
+	<tr><td><span style='color:blue;'>Salary of Mr. C is</span></td><td>".$c." $</td></tr>
+	</table>";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
